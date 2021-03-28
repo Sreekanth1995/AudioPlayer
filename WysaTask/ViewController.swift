14a3d774e83f2c1e4f77b4b4fc1f3afbef3fca4b
M Sreekanth <m.sreekanth112@gmail.com>
2021-03-28 21:46:17 +0530

    Initial commit

---
+++ WysaTask/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WysaTask
+//
+//  Created by M sreekanth  on 28/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Audio Player"
+        
+    }
+}
+
